c2b1d093dc323cffe862f156845a22862587ea7e
MaEulHP <boy123753@gmail.com>
2024-10-16 15:38:45 +0900

    playerMove
    이거 앞뒤로는 안움직임 어캄

---
+++ Source/BarkSouls/DogCharacter.cpp
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+
+#include "DogCharacter.h"
+
+// Sets default values
+ADogCharacter::ADogCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	static ConstructorHelpers::FObjectFinder<UInputMappingContext>InputMappingContext(TEXT("/Game/Kwanik/Input/IMC_Player.IMC_Player"));
+	if(InputMappingContext.Succeeded()){
+		DefaultMappingContext = InputMappingContext.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UInputAction>InputActionMove(TEXT("/Game/Kwanik/Input/IA_Move.IA_Move"));
+	if(InputActionMove.Succeeded()){
+		InputToMove = InputActionMove.Object;
+	}
+	
+	static ConstructorHelpers::FObjectFinder<UInputAction>InputActionLook(TEXT("/Game/Kwanik/Input/IA_Look.IA_Look"));
+	if(InputActionLook.Succeeded()){
+		InputToLook = InputActionLook.Object;
+	}
+
+}
+
+// Called when the game starts or when spawned
+void ADogCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	if(PlayerController != nullptr){
+		UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(
+			PlayerController->GetLocalPlayer()
+		);
+		if(Subsystem != nullptr){
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+	
+}
+
+// Called every frame
+void ADogCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ADogCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+	if(EnhancedInputComponent != nullptr){
+		EnhancedInputComponent->BindAction(InputToMove, ETriggerEvent::Triggered, this, &ADogCharacter::EnhancedInputMove);
+		EnhancedInputComponent->BindAction(InputToLook, ETriggerEvent::Triggered, this, &ADogCharacter::EnhancedInputLook);
+	}
+
+}
+
+void ADogCharacter::EnhancedInputMove(const FInputActionValue& Value){
+	FVector2D MovementVector = Value.Get<FVector2D>();
+	if(Controller != nullptr)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void ADogCharacter::EnhancedInputLook(const FInputActionValue& Value){
+	FVector2D LookVector = Value.Get<FVector2D>();
+
+	AddControllerYawInput(LookVector.X);
+	AddControllerPitchInput(LookVector.Y);
+}
---
+++ Source/BarkSouls/DogCharacter.h
@@ -0,0 +1,42 @@
+#include "InputActionValue.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+
+#pragma once
+
+#include "DogCharacter.generated.h"
+
+UCLASS()
+class BARKSOULS_API ADogCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ADogCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	//Input
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Enhanced Input")
+	class UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Enhanced Input")
+	class UInputAction* InputToMove;	
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Enhanced Input")
+	class UInputAction* InputToLook;	
+
+	void EnhancedInputMove(const FInputActionValue& Value);
+	void EnhancedInputLook(const FInputActionValue& Value);
+};
