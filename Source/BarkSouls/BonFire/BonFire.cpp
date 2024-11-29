c95e9177093e361974a4a5171cc1ea48212693bf
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2024-11-29 15:34:55 +0900

    화톳불 전송기능

--- Source/BarkSouls/BonFire/BonFire.cpp
+++ Source/BarkSouls/BonFire/BonFire.cpp
@@ -1,32 +1,69 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "BonFire.h"
+#include "Bonfire.h"
 
-ABonFire::ABonFire()
+// 정적 변수 초기화
+TMap<FName, FTransform> ABonfire::BonfireLocations;
+
+ABonfire::ABonfire()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	BonfireMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BonfireMesh"));
 	RootComponent = BonfireMesh;
 
+	InteractionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("InteractionBox"));
+	InteractionBox->SetupAttachment(RootComponent);
+
 	MapResetComponent = CreateDefaultSubobject<UMapResetComponent>(TEXT("MapResetComponet"));
+
+	bIsRegistered = false;
 }
 
-void ABonFire::BeginPlay()
+void ABonfire::BeginPlay()
 {
 	Super::BeginPlay();
 
 }
 
-// Called every frame
-void ABonFire::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
 
+void ABonfire::Interact(AActor* Player)
+{
+	if (!bIsRegistered) // 아직 등록되지 않은 경우
+	{
+		RegisterBonfireLocation();
+		bIsRegistered = true; // 등록 상태로 변경
+		UE_LOG(LogTemp, Log, TEXT("화톳불 등록 완료: %s"), *BonfireID.ToString());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Log, TEXT("이미 등록된 화톳불: %s"), *BonfireID.ToString());
+	}
+
+	// 추가적으로 UI를 열거나 효과를 실행할 수 있음
 }
 
-void ABonFire::Interact(AActor* Plyer)
+void ABonfire::RegisterBonfireLocation()
 {
-	// 
+	if (!BonfireID.IsNone())
+	{
+		BonfireLocations.Add(BonfireID, GetActorTransform());
+	}
+}
+
+void ABonfire::TeleportPlayer(AActor* Player, FName TargetBonfireID)
+{
+	if (!BonfireLocations.Contains(TargetBonfireID))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("화톳불ID 존재하지 않음!"));
+		return;
+	}
+
+	FTransform TargetTransform = BonfireLocations[TargetBonfireID];
+	Player->SetActorLocation(TargetTransform.GetLocation());
+	Player->SetActorRotation(TargetTransform.GetRotation().Rotator());
+
+	// 효과 넣으면 좋을듯.
+
 }
--- Source/BarkSouls/BonFire/BonFire.h
+++ Source/BarkSouls/BonFire/BonFire.h
@@ -5,33 +5,55 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Components/BoxComponent.h"
-#include "BonFire.generated.h"
+#include "MapResetComponent.h"
+#include "Bonfire.generated.h"
 
 UCLASS()
-class BARKSOULS_API ABonFire : public AActor
+class BARKSOULS_API ABonfire : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	ABonFire();
+
+public:
+	ABonfire();
 
 protected:
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	// 화톳불 위치 데이터
+	static TMap<FName, FTransform> BonfireLocations;
+
+	// 화톳불 등록 여부
+	bool bIsRegistered;
+
+	// 현재 화톳불 ID
+	UPROPERTY(EditAnywhere, Category = "Bonfire")
+	FName BonfireID;
+
+	// 화톳불 메시
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* BonfireMesh; 
+
+	// 화톳불 콜리전 박스
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* InteractionBox; 
+
+	UPROPERTY(EditAnywhere)
+	class UMapResetComponent* MapResetComponent;
+
+private:
+	// 다른 화톳불로 순간이동
+	UFUNCTION(BlueprintCallable, Category = "Bonfire")
+	void TeleportPlayer(AActor* Player, FName TargetBonfireID);
+
+	// 화톳불 위치 등록 -> 화톳불과 상호작용을 해야함 ID가 등록됨.
+	void RegisterBonfireLocation();
+public:
+	// 화톳불 상화작용
 	UFUNCTION()
 	void Interact(AActor* Plyer);
 
-private:
-	UPROPERTY(EditAnywhere)
-	UStaticMeshComponent* BonfireMesh; // 화톳불 메시
-
-	UPROPERTY(EditAnywhere)
-	UBoxComponent* InteractionBox;
-
-	// 기능별 컴포넌트
-	UPROPERTY(EditAnywhere)
-	class UMapResetComponent* MapResetComponent;
 };
